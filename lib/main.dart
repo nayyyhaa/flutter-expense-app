1b937b90ab2eb8c277810819e275aa44473479a3
nayyyhaa <neha@commutatus.com>
2022-11-10 00:22:44 +0530

    feat💸: new transaction modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transactions.dart';
+import './models/Transaction.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,22 +11,92 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Xpense App"),
-          backgroundColor: Color.fromARGB(255, 126, 86, 226),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: const Card(
-                child: Text("CHART!"),
-              ),
+      title: 'Expense App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Shoes',
+      amount: 70.6,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Dress',
+      amount: 75,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void addNewTransaction(String titleTx, double amountTx) {
+    final newTransaction = Transaction(
+        id: DateTime.now().toString(),
+        title: titleTx,
+        amount: amountTx,
+        date: DateTime.now());
+
+    setState(() => {_userTransactions.add(newTransaction)});
+  }
+
+  void openModalTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        context: ctx,
+        builder: (_) {
+          return GestureDetector(
+            onTap: () {},
+            child: NewTransaction(addNewTransaction),
+            behavior: HitTestBehavior.opaque,
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Xpense App"),
+        backgroundColor: Color.fromARGB(255, 126, 86, 226),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => openModalTransaction(context),
+          )
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: const Card(
+              child: Text("CHART!"),
             ),
-            UserTransactions()
-          ],
-        ),
+          ),
+          Column(
+            children: [
+              NewTransaction(addNewTransaction),
+              TransactionList(_userTransactions),
+            ],
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => openModalTransaction(context),
+        child: Icon(Icons.add),
+        backgroundColor: Color.fromARGB(255, 126, 86, 226),
+        elevation: 12,
+        hoverElevation: 30,
       ),
     );
   }
