cc47999d188f34a9f9964ee94bb01801a285286c
nayyyhaa <neha@commutatus.com>
2022-11-30 12:55:02 +0530

    style: responsive height vis LayoutBuilder

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -13,40 +13,45 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-            height: 20,
-            child: FittedBox(
-              child: Text('\$${sprendingAmt.toStringAsFixed(0)}'),
-            )),
-        SizedBox(
-          height: 10,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(children: [
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 1.0),
-                  borderRadius: BorderRadius.circular(2)),
-            ),
-            FractionallySizedBox(
-              heightFactor: spendingPtg,
-              child: Container(
+    return LayoutBuilder(builder: (BuildContext ctx, constraint) {
+      return Column(
+        children: <Widget>[
+          Container(
+              height: constraint.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text('\$${sprendingAmt.toStringAsFixed(0)}'),
+              )),
+          SizedBox(
+            height: constraint.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraint.maxHeight * 0.6,
+            width: 10,
+            child: Stack(children: [
+              Container(
                 decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColorLight,
+                    border: Border.all(color: Colors.black, width: 1.0),
                     borderRadius: BorderRadius.circular(2)),
               ),
-            )
-          ]),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        Text(label)
-      ],
-    );
+              FractionallySizedBox(
+                heightFactor: spendingPtg,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColorLight,
+                      borderRadius: BorderRadius.circular(2)),
+                ),
+              )
+            ]),
+          ),
+          SizedBox(
+            height: constraint.maxHeight * 0.05,
+          ),
+          Container(
+            child: FittedBox(child: Text(label)),
+            height: constraint.maxHeight * 0.15,
+          )
+        ],
+      );
+    });
   }
 }
